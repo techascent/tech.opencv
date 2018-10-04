7cfbe1f18e969c944a19b479c4e170e9438170bc
Chris Nuernberger <chris@thinktopic.com>
2018-10-04 13:54:08 -0600

    Release 0.1.4

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject techascent/tech.opencv "0.1.4-SNAPSHOT"
+(defproject techascent/tech.opencv "0.1.4"
   :description "Opencv bindings via javacpp"
   :url "http://github.com/tech-ascent/tech.opencv"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.javacpp-datatype "0.3.5"]
+                 [techascent/tech.javacpp-datatype "0.3.6"]
                  [org.bytedeco.javacpp-presets/opencv-platform "3.4.0-1.4"]])
