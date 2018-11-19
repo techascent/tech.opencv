40a9a415df105845d8d3633fea5e10d56f083211
Chris Nuernberger <chris@thinktopic.com>
2018-11-19 11:51:39 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.opencv "2.0"
+(defproject techascent/tech.opencv "2.1-SNAPSHOT"
   :description "Opencv bindings via javacpp"
   :url "http://github.com/tech-ascent/tech.opencv"
   :license {:name "Eclipse Public License"
