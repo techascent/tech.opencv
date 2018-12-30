58ad2253bab47f38dbb89e7fb803636fc00263ee
Chris Nuernberger <chris@thinktopic.com>
2018-12-30 15:06:28 -0700

    moved to deps.edn.

--- project.clj
+++ project.clj
@@ -3,9 +3,7 @@
   :url "http://github.com/tech-ascent/tech.opencv"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.javacpp-datatype "3.0"]
-                 [org.bytedeco.javacpp-presets/opencv-platform "3.4.0-1.4"]]
-
-  ;;Makes for some great demos
-  :profiles {:dev {:dependencies [[techascent/tech.compute "3.0"]]}})
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}})
