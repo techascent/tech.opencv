02fb3f6a636e68fd23e1459a99d93bf1985c29e8
Chris Nuernberger <chris@thinktopic.com>
2019-03-13 08:35:10 -0600

    3.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.opencv "3.6-SNAPSHOT"
+(defproject techascent/tech.opencv "3.6"
   :description "Opencv bindings via javacpp"
   :url "http://github.com/tech-ascent/tech.opencv"
   :license {:name "Eclipse Public License"
