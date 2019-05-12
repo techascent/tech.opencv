b796d248aa11ab696392ded37c8432731d9500bd
Chris Nuernberger <chris@techascent.com>
2019-05-12 14:11:03 -0600

    upgraded to latest datatype.

--- test/tech/opencv_compute_test.clj
+++ test/tech/opencv_compute_test.clj
@@ -1,12 +1,11 @@
 (ns tech.opencv-compute-test
   (:require [tech.opencv :as opencv]
-            [tech.compute.tensor :as ct]
             [tech.resource :as resource]
             [clojure.test :refer :all]
-            [tech.compute.cpu.tensor-math :as cpu-tm]
             [tech.opencv-test :as opencv-test]
-            [tech.compute.tensor.operations :as op]
-            [tech.datatype.jna :as dtype-jna]))
+            [tech.v2.datatype :as dtype]
+            [tech.v2.datatype.functional :as dfn]
+            [tech.v2.tensor :as dtt]))
 
 
 (deftest bgr-test
@@ -14,31 +13,33 @@
   (resource/stack-resource-context
     (let [test-image (opencv/load "test/data/test.jpg")
           ;;Select is in-place so this did not change the image at all.
-          bgr-image (ct/select test-image :all :all [2 1 0])
-          dest-tens (ct/clone bgr-image)]
+          bgr-image (dtt/select test-image :all :all [2 1 0])
+          dest-image (dtype/copy! bgr-image (dtype/from-prototype test-image))]
       ;;The tensor library has the convention that the thing that is mutated
       ;;is the first thing.  Also the thing that is mutated is returned from
       ;;the function.
-      (ct/assign! test-image dest-tens)
-      (opencv/save test-image "bgr.jpg"))))
+      (opencv/save dest-image "bgr.jpg"))))
 
 
 (deftest lighten-bgr-test
   []
   (resource/stack-resource-context
-    (let [test-image (opencv/load "test/data/test.jpg")]
-      (ct/assign! test-image (-> test-image
-                                 (ct/select :all :all [2 1 0])
-                                 (ct/clone :datatype :uint16)
-                                 (op/+ 50)
-                                 ;;Clamp top end to 0-255
-                                 (op/min 255)))
+   (let [test-image (opencv/load "test/data/test.jpg")
+         result
+         (-> test-image
+             (dtt/select :all :all [2 1 0])
+             (dfn/+ 50)
+             ;;Clamp top end to 0-255
+             (dfn/min 255)
+             (dtype/copy! (dtype/from-prototype test-image)))]
 
-      (opencv/save test-image "bgr-lighten.jpg"))))
+      (opencv/save result "bgr-lighten.jpg"))))
 
 
 (deftest smooth-image-flow
   (resource/stack-resource-context
-    (-> (opencv/load "test/data/test.jpg")
-        (op// 2)
-        (opencv/save "tensor_darken.jpg"))))
+   (let [src-img (opencv/load "test/data/test.jpg")]
+     (-> src-img
+         (dfn// 2)
+         (dtype/copy! (dtype/from-prototype src-img))
+         (opencv/save "tensor_darken.jpg")))))
