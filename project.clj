234150c646030ebfa0aa5c469f2882f915f34655
Chris Nuernberger <chris@techascent.com>
2019-05-26 10:31:55 -0600

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.opencv "4.0-alpha37"
+(defproject techascent/tech.opencv "4.0-alpha37-1-SNAPSHOT"
   :description "Opencv bindings via javacpp"
   :url "http://github.com/tech-ascent/tech.opencv"
   :license {:name "Eclipse Public License"
